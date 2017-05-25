2c27092729894c7fd76fc4112d3360cd212a2cbe
jsantos737 <joaoantonios74@gmail.com>
2017-05-25 00:57:34 -0300

    inciando o chat

--- chat/index.php
+++ chat/index.php
@@ -1,3 +1,18 @@
 <?php
-    
+    session_start();
+    if ($_SESSION['status']==1){
+        echo "logado";
+    }else{
+        echo "nao logado";
+        }
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <link rel="stylesheet" type="text/css" href="../estilo.css">
+    </head>
+	<?php include '../header.php'; ?>
+		
+		
+	<?php include '../footer.php'; ?>
+</html>
--- menu.php
+++ menu.php
@@ -5,12 +5,14 @@
 				';
 	if ($_SESSION['status']==1){
 		echo '<li><a href="';include 'servidor.php';echo '">Inicio</a></li>';
+		echo '<li><a href="';include 'servidor.php';echo 'chat/">Chat</a></li>';
 		echo 
 			'<li>
 				<a>Conta</a>
 				<ul>
 					<li><a href="';include 'servidor.php';echo 'conta/">Dados</a></li>
 					<li><a href="';include 'servidor.php';echo 'sair.php">Sair</a></li>
+					
 				</ul>
 			</li>';
 	}
